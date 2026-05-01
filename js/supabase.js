0bc78458bec25059152ae92c1009799f2ff782eb
tuttovinilos-cloud <tuttovinilos@gmail.com>
2026-05-01 12:16:32 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Conexión a Supabase
 
-const SUPABASE_URL = "TU_URL_AQUI";
+const SUPABASE_URL = "https://ujfdsabypflseijtaxba.supabase.co";
 const SUPABASE_KEY = "sb_publishable_CiZo8xFKkaT7mdRrQUYxuQ_48F6Vx3w";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
